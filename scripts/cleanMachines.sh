39ccd827885f2fceb726ec3dc9a94535c15330be
Ritesh Agarwal <ritesh.agarwal@couchbase.com>
2017-12-16 23:11:07 +0530

    breaking dp3 jobs into 2 to debug.

--- scripts/cleanMachines.sh
+++ scripts/cleanMachines.sh
@@ -1,21 +1,8 @@
-kill -9 $(ps aux | grep 'couchdb' |  awk '{print $2}')
-dpkg -r couchbase-server-analytics;dpkg --purge couchbase-server-analytics;
-kill -9 $(ps aux | grep 'epmd' |  awk '{print $2}')
-kill -9 $(ps aux | grep 'beam.smp' |  awk '{print $2}')
-kill -9 $(ps aux | grep 'memsup' |  awk '{print $2}')
-kill -9 $(ps aux | grep 'memcached' |  awk '{print $2}') 
-kill -9 $(ps aux | grep 'cpu_sup' |  awk '{print $2}')
-kill -9 $(ps aux | grep 'moxi' |  awk '{print $2}') 
-kill -9 $(ps aux | grep 'goxdcr' |  awk '{print $2}')
-kill -9 $(ps aux | grep 'vbucketmigrator' |  awk '{print $2}') 
-kill -9 $(ps aux | grep 'erlang' |  awk '{print $2}')
-kill -9 $(ps aux | grep 'cpu_sup' |  awk '{print $2}') 
-rm -rf /var/opt/membase
-rm -rf /opt/membase
-rm -rf /etc/opt/membase
-rm -rf /var/membase/data/*
-rm -rf /opt/membase/var/lib/membase/*
-rm -rf /opt/couchbase
-rm -rf /data/*
-ipcrm
-dpkg -i /tmp/couchbase-server-analytics_5.1.0-917-ubuntu14.04_amd64.deb 
+
+rm -rf /Applications/Couchbase\ Server.app
+rm -rf ~/Library/Application\ Support/Couchbase
+rm -rf ~/Library/Application\ Support/Membase
+
+system_profiler SPSoftwareDataType
+
+#echo "couchbase" | sudo shutdown -r now
