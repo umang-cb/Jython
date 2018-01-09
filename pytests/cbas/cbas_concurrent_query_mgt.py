42e808890790ea624ad4faceaffbebfc166fe91c
Ritesh Agarwal <ritesh.agarwal@couchbase.com>
2018-01-09 09:33:56 +0530

    Correcting the infinite loop in concurrency tests in cbas.

--- pytests/cbas/cbas_concurrent_query_mgt.py
+++ pytests/cbas/cbas_concurrent_query_mgt.py
@@ -46,12 +46,13 @@ class CBASConcurrentQueryMgtTests(CBASBaseTest):
                                                self.num_items)
 
         # Wait while ingestion is completed
-        total_items, _ = self.cbas_util.get_num_items_in_cbas_dataset(
-            self.cbas_dataset_name)
-        while (self.num_items > total_items):
-            self.sleep(5)
-            total_items, _ = self.cbas_util.get_num_items_in_cbas_dataset(
-                self.cbas_dataset_name)
+        self.cbas_util.wait_for_ingestion_complete([self.cbas_dataset_name],self.num_items)
+#         total_items, _ = self.cbas_util.get_num_items_in_cbas_dataset(
+#             self.cbas_dataset_name)
+#         while (self.num_items > total_items):
+#             self.sleep(5)
+#             total_items, _ = self.cbas_util.get_num_items_in_cbas_dataset(
+#                 self.cbas_dataset_name)
         if self.mode is not None:
             self.cbas_util.closeConn()
 
