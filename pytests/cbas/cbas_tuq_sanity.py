7628f8b2598aed76d72104cdede7f734a618068e
Ritesh Agarwal <ritesh.agarwal@couchbase.com>
2018-03-23 16:10:38 +0530

    Enabling and adding tests for Case Expressions for analytics.
    Change-Id: I456ecacfc63880c77107e393c7cbf26035b1453e
    Reviewed-on: http://review.couchbase.org/91506
    Reviewed-by: <tanzeem.ahmed@couchbase.com>
    Tested-by: <tanzeem.ahmed@couchbase.com>

--- pytests/cbas/cbas_tuq_sanity.py
+++ pytests/cbas/cbas_tuq_sanity.py
@@ -188,3 +188,59 @@ class CBASTuqSanity(QuerySanityTests):
                                for doc in self.full_list]
             expected_result = sorted(expected_result)
             self._verify_results(actual_result, expected_result)
+
+    def test_case_and_like(self):
+        for bucket in self.buckets:
+            self.query = "SELECT name, CASE WHEN join_mo < 3 OR join_mo > 11 THEN" +\
+            " 'winter' ELSE 'other' END AS period FROM %s WHERE (CASE WHEN" % (bucket.name) +\
+            " join_mo < 3 OR join_mo > 11 THEN 'winter' ELSE 'other' END) LIKE 'win%'"
+            actual_result = self.run_cbq_query()
+            actual_result = sorted(actual_result['results'], key=lambda doc: (
+                                                                doc['name'],doc['period']))
+            expected_result = [{"name" : doc['name'],
+                                "period" : ('other','winter')
+                                            [doc['join_mo'] in [12,1,2]]}
+                               for doc in self.full_list
+                               if ('other','winter')[doc['join_mo'] in [12,1,2]].startswith(
+                                                                                'win')]
+            expected_result = sorted(expected_result, key=lambda doc: (doc['name'],
+                                                                       doc['period']))
+            self._verify_results(actual_result, expected_result)
+
+    def test_case_and_logic_exp(self):
+        for bucket in self.buckets:
+            self.query = "SELECT DISTINCT name, CASE WHEN join_mo < 3 OR join_mo > 11 THEN" +\
+            " 'winter' ELSE 'other' END AS period FROM %s WHERE (CASE WHEN join_mo < 3" %(bucket.name) +\
+            " OR join_mo > 11 THEN 1 ELSE 0 END) > 0 AND job_title='Sales'"
+            actual_result = self.run_cbq_query()
+            actual_result = sorted(actual_result['results'], key=lambda doc: (
+                                                                       doc['name'],doc['period']))
+            expected_result = [{"name" : doc['name'],
+                                "period" : ('other','winter')
+                                            [doc['join_mo'] in [12,1,2]]}
+                               for doc in self.full_list
+                               if (0, 1)[doc['join_mo'] in [12,1,2]] > 0 and\
+                                  doc['job_title'] == 'Sales']
+            expected_result = [dict(y) for y in set(tuple(x.items()) for x in expected_result)]
+            expected_result = sorted(expected_result, key=lambda doc: (doc['name'],
+                                                                       doc['period']))
+            self._verify_results(actual_result, expected_result)
+
+    def test_case_and_comparision_exp(self):
+        for bucket in self.buckets:
+            self.query = "SELECT DISTINCT name, CASE WHEN join_mo < 3 OR join_mo > 11 THEN" +\
+            " 'winter' ELSE 'other' END AS period FROM %s WHERE (CASE WHEN join_mo < 3" %(bucket.name) +\
+            " OR join_mo > 11 THEN 1 END) = 1 AND job_title='Sales'"
+            actual_result = self.run_cbq_query()
+            actual_result = sorted(actual_result['results'], key=lambda doc: (
+                                                                       doc['name'],doc['period']))
+            expected_result = [{"name" : doc['name'],
+                                "period" : ('other','winter')
+                                            [doc['join_mo'] in [12,1,2]]}
+                               for doc in self.full_list
+                               if (doc['join_mo'], 1)[doc['join_mo'] in [12,1,2]] == 1 and\
+                                  doc['job_title'] == 'Sales']
+            expected_result = [dict(y) for y in set(tuple(x.items()) for x in expected_result)]
+            expected_result = sorted(expected_result, key=lambda doc: (doc['name'],
+                                                                       doc['period']))
+            self._verify_results(actual_result, expected_result)
